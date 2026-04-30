045dfd92aa734409d84993393183cc4a88c79be8
Jacob Scholl <jacobscholl314@gmail.com>
2026-04-29 19:08:13 -0500

    added stop to stopwatch lol

--- utils/other/stopwatch.hpp
+++ utils/other/stopwatch.hpp
@@ -2,16 +2,28 @@
 
 #include <chrono>
 
-struct StopWatch {
+class StopWatch {
+  public:
     using clock = std::chrono::steady_clock;
 
     StopWatch() : start_time(clock::now()) {}
 
     double seconds() const {
-        return std::chrono::duration<double>{clock::now() - start_time}.count();
+        if (stop_time != clock::time_point{}) {
+            return std::chrono::duration<double>{stop_time - start_time}.count();
+        } else {
+            return std::chrono::duration<double>{clock::now() - start_time}.count();
+        }
     }
 
-    void restart() { start_time = clock::now(); }
+    void restart() {
+        stop_time = clock::time_point{};
+        start_time = clock::now();
+    }
 
+    void stop() { stop_time = clock::now(); }
+
+  private:
     clock::time_point start_time;
+    clock::time_point stop_time{};
 };
